4cc0796169962eae8abb0999b2a50b391a10c5e6
Oskar Drescher <drescher.oskar@gmail.com>
2021-10-13 21:49:39 +0200

    extract calculation to object

---
+++ user-service/src/main/java/pl/odsoftware/userservice/domain/Calculation.java
@@ -0,0 +1,25 @@
+package pl.odsoftware.userservice.domain;
+
+public class Calculation {
+
+    private final double result;
+
+    Calculation(int followers, int publicRepos) {
+        this.result = execute(followers, publicRepos);
+    }
+
+    private double execute(int followers, int publicRepos){
+        if(noFollowers(followers)){
+            return 0.00;
+        }
+        return 6.0 / followers * (2.0 + publicRepos);
+    }
+
+    private boolean noFollowers(int followers) {
+        return followers == 0;
+    }
+
+    public double result() {
+        return result;
+    }
+}
