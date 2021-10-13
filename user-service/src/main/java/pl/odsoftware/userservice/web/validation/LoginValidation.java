b4d799c58db9f1b0f6feed8a212689bb25f5e0d3
Oskar Drescher <drescher.oskar@gmail.com>
2021-10-13 20:54:02 +0200

    validation for github users

---
+++ user-service/src/main/java/pl/odsoftware/userservice/web/validation/LoginValidation.java
@@ -0,0 +1,7 @@
+package pl.odsoftware.userservice.web.validation;
+
+public class LoginValidation {
+
+    public static final String GITHUB_LOGIN = "^(?=.*[A-Za-z0-9]$)[A-Za-z][A-Za-z\\d.-]{0,38}$";
+
+}
